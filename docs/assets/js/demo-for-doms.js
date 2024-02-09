445dc799e81660d91c76fc241e4ac8e868b046cc
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2024-02-09 11:53:18 +0000

    Update demo-for-doms.js

--- docs/assets/js/demo-for-doms.js
+++ docs/assets/js/demo-for-doms.js
@@ -15,3 +15,34 @@ document.addEventListener(
   },
   false,
 );
+
+
+// code source https://github.com/evg-zhabotinsky/RandomStuff/blob/gh-pages/webexperiments/fullscreen/w3s.html
+// note, page needs a div with id='main_frame'
+function goFullscreen() {
+  // Must be called as a result of user interaction to work
+  mf = document.getElementById("main_frame");
+  mf.webkitRequestFullscreen();
+  mf.style.display = "";
+}
+
+function fullscreenChanged() {
+  if (document.webkitFullscreenElement == null) {
+    mf = document.getElementById("main_frame");
+    mf.style.display = "none";
+  }
+}
+
+document.onwebkitfullscreenchange = fullscreenChanged;
+document.documentElement.onclick = goFullscreen;
+document.onkeydown = goFullscreen;
+
+
+// source code https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+addEventListener("mousemove", (event) => {});
+
+onmousemove = (event) => {
+  alert('You move your mouse!\nPress ESC to get out ot this!');
+  fullscreenChanged;
+  goFullscreen;
+};
