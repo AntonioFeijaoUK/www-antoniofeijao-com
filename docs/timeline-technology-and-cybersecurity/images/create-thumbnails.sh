e42c8dc24333e8662f5141203dccb3b8a5ec1b0f
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2024-08-03 02:27:45 +0100

    timeline version 2.0 - improved dataset and added filters

--- docs/timeline-technology-and-cybersecurity/images/create-thumbnails.sh
+++ docs/timeline-technology-and-cybersecurity/images/create-thumbnails.sh
@@ -10,15 +10,19 @@
 
 cat ../events/event-* | jq -r '.events[].image' | cut -f 3 -d '/' > list-of-images.txt
 
+convert placeholder-image-for-all.webp  -thumbnail 64x64 thumbnail.jpg
+
 # Iterate over each line in the list-of-images.txt file
 for image in $(cat list-of-images.txt); do
     # Print the name of the image being processed
     echo "Processing image: ${image}"
     
-    # Copy the placeholder thumbnail to the destination directory with the name of the current image
-    #cp thumbnail.jpg ../images-thumbnails/${image}
+    cp placeholder-image-for-all.webp ${image}
 
-    convert ${image} -thumbnail 200x200 ../images-thumbnails/${image}
+    # Copy the placeholder thumbnail to the destination directory with the name of the current image
+    cp thumbnail.jpg ../images-thumbnails/${image}
+
+    #convert ${image} -thumbnail 200x200 ../images-thumbnails/${image}
 
 done
 
